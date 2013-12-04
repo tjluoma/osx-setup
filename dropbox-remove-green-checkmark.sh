8c6d27a0226214033866cb6f4235787c354b537d
TJ Luoma <github@luo.ma>
2013-12-04 12:26:05 -0500

    Update dropbox-remove-green-checkmark.sh
    updated path for 'green check file'

--- dropbox-remove-green-checkmark.sh
+++ dropbox-remove-green-checkmark.sh
@@ -1,4 +1,9 @@
 # Remove Dropbox’s green checkmark icons in Finder.app
-file=/Applications/Dropbox.app/Contents/Resources/check.icns
+
+file='/Applications/Dropbox.app/Contents/Resources/emblem-dropbox-uptodate.icns'
+
 [ -e "$file" ] && mv -f "$file" "$file.bak"
 unset file
+
+# note: This file used to be
+# file=/Applications/Dropbox.app/Contents/Resources/check.icns
